f9ae3a962aaa739a358aa98f90614e8cb4a32009
Sravana Thatta <sravana.thatta@rbs.co.uk>
2020-02-18 20:48:24 +0000

    NumbersFun: Added failing test for invalid input

--- src/test/java/org/fun/test/NumbersFunTest.java
+++ src/test/java/org/fun/test/NumbersFunTest.java
@@ -7,16 +7,21 @@ import org.junit.jupiter.api.Test;
 
 public class NumbersFunTest {
 
+    NumbersFun numbersFun = new NumbersFun();
+
     @Test
     public void when_the_number_is_3_then_the_application_should_print_fizz(){
-        NumbersFun numbersFun = new NumbersFun();
         assertEquals("Fizz", numbersFun.getNumber(3));
     }
 
     @Test
     public void when_the_number_is_divisible_by_3_then_the_application_should_print_fizz(){
-        NumbersFun numbersFun = new NumbersFun();
         assertEquals("Fizz", numbersFun.getNumber(6));
+    }
+
+    @Test
+    public void when_the_number_is_negative_number_then_error_should_be_returned(){
+        assertEquals("Invalid Input. Number to should 0 or greater", numbersFun.getNumber(-1));
     }
 
 }
