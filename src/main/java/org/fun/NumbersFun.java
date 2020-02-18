634d87e3408f70e9a6138e3771e1e5e53dc34671
Sravana Thatta <sravana.thatta@rbs.co.uk>
2020-02-18 20:40:40 +0000

    NumbersFun: Fixed the fail test when the number is 3

---
+++ src/main/java/org/fun/NumbersFun.java
@@ -0,0 +1,8 @@
+package org.fun;
+
+public class NumbersFun {
+
+    public String getNumber(int number){
+        return "Fizz";
+    }
+}
--- src/test/java/org/fun/test/NumbersFunTest.java
+++ src/test/java/org/fun/test/NumbersFunTest.java
@@ -1,13 +1,15 @@
-package org.numbersfun.test;
+package org.fun.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.fun.NumbersFun;
 import org.junit.jupiter.api.Test;
 
 public class NumbersFunTest {
 
     @Test
     public void when_the_number_is_divisible_by_3_then_the_application_should_print_fizz(){
-        NumbersFun numbersFun = NumbersFunTest();
+        NumbersFun numbersFun = new NumbersFun();
         assertEquals("Fizz", numbersFun.getNumber(3));
     }
 
