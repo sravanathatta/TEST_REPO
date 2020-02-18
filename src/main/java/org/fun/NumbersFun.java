d293f34b1d1f0fc91194dbc576e9a6a4d8ceaaba
Sravana Thatta <sravana.thatta@rbs.co.uk>
2020-02-18 20:49:13 +0000

    NumbersFun: Fixed invalid input

--- src/main/java/org/fun/NumbersFun.java
+++ src/main/java/org/fun/NumbersFun.java
@@ -3,6 +3,10 @@ package org.fun;
 public class NumbersFun {
 
     public String getNumber(int number){
+        if(number <0 ){
+            return "Invalid Input. Number to should 0 or greater";
+        }
+
         if(number%3==0){
             return "Fizz";
         }
